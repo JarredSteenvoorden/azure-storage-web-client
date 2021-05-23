afea21073498ca6d4c09dbda587997d66edfcd72
Jarred Steenvoorden <jarred.steenvoorden@fetechgroup.com>
2021-05-23 22:30:24 +1000

    Add external config, copy to clipboard, read from anon storage account

--- azure-storage-web-client/src/components/HelloWorld.vue
+++ azure-storage-web-client/src/components/HelloWorld.vue
@@ -1,22 +1,40 @@
 <template>
   <v-container>
+    <div class="text-h2">{{ config.header }}</div>
+
+    <div>
+      <router-link to="/">home/</router-link>
+    </div>
+
     <v-data-table
+        :loading="loading"
         :headers="headers"
         :items="desserts"
         :items-per-page="-1"
         :hide-default-footer="true"
         class="elevation-1"
+        loading-text="Loading..."
     >
       <template v-slot:item.name="{item}">
-        <router-link :to="`/${item.containerName}/${item.prefix}`">{{item.name}}</router-link>
+        <router-link v-if="item.isFolder" :to="`/${item.containerName}/${item.prefix}`">{{ item.name }}</router-link>
+        <a v-if="!item.isFolder" :href="`${config.downloadUri}/${item.containerName}/${item.prefix}`">{{ item.name }}</a>
       </template>
       <template v-slot:item.size="{item}">
-        <div>{{!item.isFolder ? humanFileSize(item.size) : null}}</div>
+        <div>{{ !item.isFolder ? humanFileSize(item.size) : null }}</div>
       </template>
       <template v-slot:item.lastModified="{item}">
-        <div>{{!item.isFolder ? format(item.lastModified, 'yyyy-MM-dd hh:mm:ss a') : null}}</div>
+        <div>{{ !item.isFolder ? format(item.lastModified, 'yyyy-MM-dd hh:mm:ss a') : null }}</div>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <v-icon v-if="!item.isFolder" small class="mr-2" @click="copyToClipboard(item.prefix)">mdi-link</v-icon>
       </template>
     </v-data-table>
+
+    <v-snackbar
+        v-model="snackbar"
+        timeout="2000"
+    > Copied to clipboard
+    </v-snackbar>
   </v-container>
 </template>
 
@@ -24,8 +42,7 @@
   const { BlobServiceClient } = require("@azure/storage-blob");
   import { format } from 'date-fns'
 
-  const connectionString = 'ENTER_CONNECTION_STRING';
-  const blobServiceClient = new BlobServiceClient(connectionString);
+  let blobServiceClient = null;
 
   export default {
     name: 'HelloWorld',
@@ -37,6 +54,11 @@
     },
 
     async mounted() {
+      const runtimeConfig = await fetch('/config.json')
+      this.config = await runtimeConfig.json();
+
+      blobServiceClient = new BlobServiceClient(this.config.connectionString);
+
       await this.loadPath(this.$route.path);
     },
 
@@ -53,7 +75,13 @@
       },
 
       async listContainers() {
-        let containers = blobServiceClient.listContainers();
+        //let containers = blobServiceClient.listContainers();
+        let containers = [
+          {name: 'library-software'},
+          {name: 'public'},
+          {name: 'support'},
+          {name: 'temp'},
+        ]
 
         this.desserts = [];
         for await (const container of containers) {
@@ -73,6 +101,8 @@
         if (prefix === '')
           prefix = null;
 
+        this.loading = true
+
         const containerClient = blobServiceClient.getContainerClient(containerName);
         let blobs = containerClient.listBlobsByHierarchy('/', {prefix: prefix});
 
@@ -88,6 +118,23 @@
             lastModified: blob.properties?.lastModified
           });
         }
+
+        this.loading = false
+      },
+
+      copyToClipboard(text) {
+        this.snackbar = true
+
+        let dummy = document.createElement("textarea");
+        // to avoid breaking orgain page when copying more words
+        // cant copy when adding below this code
+        // dummy.style.display = 'none'
+        document.body.appendChild(dummy);
+        //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
+        dummy.value = text;
+        dummy.select();
+        document.execCommand("copy");
+        document.body.removeChild(dummy);
       },
 
       humanFileSize(bytes, si) {
@@ -113,10 +160,19 @@
     data () {
       return {
         format,
+        config: {
+          header: null,
+          connectionString: null,
+          containers: [],
+          downloadUri: null
+        },
+        loading: false,
+        snackbar: false,
         headers: [
           { text: 'Name', value: 'name' },
-          { text: 'Size', value: 'size' },
-          { text: 'Last Modified', value: 'lastModified' }
+          { text: 'Size', value: 'size', width: 100 },
+          { text: 'Last Modified', value: 'lastModified', width: 200 },
+          { text: 'Actions', value: 'actions', sortable: false, width: 100 }
         ],
         desserts: [ ],
       }
--- azure-storage-web-client/vue.config.js
+++ azure-storage-web-client/vue.config.js
@@ -1,5 +1,25 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+
+  // Below required to flatten dist directory structure for hosting in the $root Azure Storage container
+  chainWebpack: (config) => {
+    config.module
+        .rule('images')
+        .use('url-loader')
+        .tap(options => Object.assign({}, options, { name: '[name].[ext]' }));
+  },
+  css: {
+    extract: {
+      filename: '[name].[hash].css',
+      chunkFilename: '[name].[hash].css',
+    },
+  },
+  configureWebpack: {
+    output: {
+      filename: '[name].[hash].js',
+      chunkFilename: '[name].[hash].js',
+    }
+  }
 }
