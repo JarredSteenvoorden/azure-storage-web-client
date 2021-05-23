90ea31ab6a02ef6ab05d4cadd02c2e3a3b2a3868
Jarred Steenvoorden <jarred.steenvoorden@fetechgroup.com>
2021-05-23 16:25:21 +1000

    List all containers and blobs on account

--- azure-storage-web-client/src/components/HelloWorld.vue
+++ azure-storage-web-client/src/components/HelloWorld.vue
@@ -1,58 +1,125 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-data-table
+        :headers="headers"
+        :items="desserts"
+        :items-per-page="-1"
+        :hide-default-footer="true"
+        class="elevation-1"
+    >
+      <template v-slot:item.name="{item}">
+        <router-link :to="`/${item.containerName}/${item.prefix}`">{{item.name}}</router-link>
+      </template>
+      <template v-slot:item.size="{item}">
+        <div>{{!item.isFolder ? humanFileSize(item.size) : null}}</div>
+      </template>
+      <template v-slot:item.lastModified="{item}">
+        <div>{{!item.isFolder ? format(item.lastModified, 'yyyy-MM-dd hh:mm:ss a') : null}}</div>
+      </template>
+    </v-data-table>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  const { BlobServiceClient } = require("@azure/storage-blob");
+  import { format } from 'date-fns'
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  const connectionString = 'ENTER_CONNECTION_STRING';
+  const blobServiceClient = new BlobServiceClient(connectionString);
+
+  export default {
+    name: 'HelloWorld',
+
+    watch: {
+      $route(to) {
+        this.loadPath(to.path);
+      }
+    },
+
+    async mounted() {
+      await this.loadPath(this.$route.path);
+    },
+
+    methods: {
+      async loadPath(path) {
+        let containerName = path.split('/')[1];
+        let prefix = path.split('/').slice(2, -1).join('/') + '/';
+        if (prefix === '/') prefix = '';
+
+        if (!containerName)
+          await this.listContainers();
+        else
+          await this.listBlobs(containerName, prefix)
+      },
+
+      async listContainers() {
+        let containers = blobServiceClient.listContainers();
+
+        this.desserts = [];
+        for await (const container of containers) {
+          this.desserts.push({
+            containerName: container.name,
+            prefix: '',
+            isFolder: true,
+
+            name: container.name,
+            size: null,
+            lastModified: null
+          })
+        }
+      },
+
+      async listBlobs(containerName, prefix = null) {
+        if (prefix === '')
+          prefix = null;
+
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+        let blobs = containerClient.listBlobsByHierarchy('/', {prefix: prefix});
+
+        this.desserts = [];
+        for await (const blob of blobs) {
+          this.desserts.push({
+            containerName: containerName,
+            prefix: blob.name,
+            isFolder: blob.kind === "prefix",
+
+            name: blob.name.replace(prefix, ''),
+            size: blob.properties?.contentLength,
+            lastModified: blob.properties?.lastModified
+          });
+        }
+      },
+
+      humanFileSize(bytes, si) {
+        if (si === undefined) si = true;
+
+        let thresh = si ? 1000 : 1024;
+        if(Math.abs(bytes) < thresh) {
+          return bytes + ' B';
+        }
+        let units = si
+            ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+            : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+        let u = -1;
+        do {
+          bytes /= thresh;
+          ++u;
+        } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+
+        return bytes.toFixed(1) + ' ' + units[u];
+      }
+    },
+
+    data () {
+      return {
+        format,
+        headers: [
+          { text: 'Name', value: 'name' },
+          { text: 'Size', value: 'size' },
+          { text: 'Last Modified', value: 'lastModified' }
+        ],
+        desserts: [ ],
+      }
+    },
+  }
+</script>
--- azure-storage-web-client/src/main.js
+++ azure-storage-web-client/src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+
 new Vue({
-  render: h => h(App),
+  router,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
