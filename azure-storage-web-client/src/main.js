a1d898d4fa3ea7f963ba9f4c2ef630207ad95b38
Jarred Steenvoorden <jarred.steenvoorden@fetechgroup.com>
2021-05-23 14:32:29 +1000

    Reset to vue 2.0

--- azure-storage-web-client/src/main.js
+++ azure-storage-web-client/src/main.js
@@ -1,8 +1,8 @@
-import { createApp } from 'vue'
-import vuetify from './plugins/vuetify'
+import Vue from 'vue'
 import App from './App.vue'
 
-const app = createApp(App)
-app.use(vuetify)
+Vue.config.productionTip = false
 
-app.mount('#app')
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
