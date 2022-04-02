1fd38231f4cc52e95bc7983490a1f6ec2b9082eb
Jarred Steenvoorden <jazzjarred@gmail.com>
2022-04-02 20:20:03 +1100

    add quasar

--- web-client-vue3/azure-storage-web-client/src/main.ts
+++ web-client-vue3/azure-storage-web-client/src/main.ts
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Import Quasar icon libraries and css
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+
+const app = createApp(App);
+
+app.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+})
+
+app.mount('#app');
